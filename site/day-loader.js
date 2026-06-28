eba41f83bb7b00285b54ba08f90b73af22a4d9a5
Daniel <daniel@Daniels-MBP.attlocal.net>
2026-06-28 15:30:43 -0700

    Revert "Refactor site into modules and update army search"
    This reverts commit eef13dbdf4ad950aff27538ddd3c1ca53a953a3c.

--- site/day-loader.js
+++
@@ -1,7 +0,0 @@
-export async function loadDayData(state, day) {
-  const item = state.dayItems.find(entry => entry.date === day);
-  if (!item) return null;
-  const res = await fetch(`./${item.payload}`, { cache: 'no-store' });
-  state.siteData = await res.json();
-  return state.siteData;
-}
